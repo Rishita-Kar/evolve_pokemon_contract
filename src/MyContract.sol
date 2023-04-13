cd333ebe8a32f76c9005c403afb7788c67329a7f
Rishita Kar <rishita.kar@instrument.com>
2023-04-13 04:02:10 -0700

    Create evolve pokemon contract

--- src/MyContract.sol
+++ src/MyContract.sol
@@ -3,11 +3,20 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
 
-contract MyContract is ERC1155LazyMint {
+contract PokemonEvolveContract is ERC1155LazyMint {
     constructor(
         string memory _name,
-        string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps
-    ) ERC1155LazyMint(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+        string memory _symbol
+    ) ERC1155LazyMint(_name, _symbol, msg.sender, 0) {}
+
+    function verifyClaim(address _claimer, uint256 _tokenId, uint256 _quantity) public view override {
+        require(_tokenId == 0, "Only Mew are claimable!");
+        require(_quantity == 1, "Only one Mew can be claimed!");
+    }
+
+    function evolve() public {
+        _burn(msg.sender, 0, 2);
+        _mint(msg.sender, 1, 1, "");
+
+    }
 }
